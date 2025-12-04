996443c6c0a8fe1613984c331bfe9273f2021341
xQuiets <116582621+xQuiets@users.noreply.github.com>
2025-12-04 10:22:20 +0400

    исходный

---
+++ MAOptimizationTask/MAOptimizationTask/MAOptimizationTask.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
+
+
+bool isPrimeBase(int n)
+{
+    if (n < 2) return false;
+    if (n == 2) return true;
+    for (int i = 3; i < n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
+std::vector<int> primesArray(int min, int max, int count)
+{
+    std::vector<int> primes;
+    srand(0);
+    for (int i = 0; i < count; i++)
+    {
+        int value = (rand()/32767.0*(max-min) + min);
+        if (isPrimeBase(value))
+            primes.push_back(value);
+    }
+    return primes;
+}
+
+int main()
+{
+    auto start = clock();
+    auto result = primesArray(8000000, 9000000, 10000);
+    auto duration = clock() - start;
+    std::cout << "Primes count: " << result.size() << " duration: " << duration << " msec.\n";
+}
